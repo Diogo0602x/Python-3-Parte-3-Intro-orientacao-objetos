4bf7e28045819e349f2a7461ef7fdfa25b28dc07
Diogo0602x <diogoteixeira0602@gmail.com>
2021-06-16 10:55:23 -0300

    Aula 1 - Atividade 03 terminada

--- teste.py
+++ teste.py
@@ -1,4 +1,12 @@
 def cria_conta(numero, titular, saldo, limite):
    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
    return conta
-   
+
+def deposita(conta, valor):
+  conta["saldo"] += valor 
+
+def saca(conta, valor):
+  conta["saldo"] -= valor
+
+def extrato(conta):
+  print("Saldo é ".format(conta["saldo"]))
