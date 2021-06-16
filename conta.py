84fb2d2e3e7dd5fb68a10f453ed81775b267d633
Diogo0602x <diogoteixeira0602@gmail.com>
2021-06-16 11:53:08 -0300

    Aula 2 - Atividade 03 terminada

--- conta.py
+++ conta.py
@@ -1,3 +1,8 @@
 class  Conta:
 
-  pass
+  def __init__(self):
+    print("Contruindo objeto ... {}").format(self))
+    self.numero = 123
+    self.titular = "Nico"
+    self.saldo = 55.0
+    self.limite = 1000.0
