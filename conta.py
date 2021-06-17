4b63f43565e26def27dfe37ed06f2c497337b134
Diogo0602x <diogoteixeira0602@gmail.com>
2021-06-17 09:53:22 -0300

    Aula 4 - Atividade 01 terminada

--- conta.py
+++ conta.py
@@ -2,16 +2,16 @@ class  Conta:
 
   def __init__(self, numero, titular, saldo, limite):
     print("Contruindo objeto ... {}".format(self))
-    self.numero = numero
-    self.titular = titular
-    self.saldo = saldo
-    self.limite = limite
+    self.__numero = numero
+    self.__titular = titular
+    self.__saldo = saldo
+    self.__limite = limite
 
   def extrato(self):
-    print("Saldo de {} de titular {}".format(self.saldo, self.titular))
+    print("Saldo de {} de titular {}".format(self.__saldo, self.__titular))
   
   def deposita(self, valor):
-    self.saldo += valor
+    self.__saldo += valor
 
   def saca(self, valor):
-    self.saldo -= valor
+    self.__saldo -= valor
