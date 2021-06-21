84c71832772f5d75775848f811dada1a55c345ed
Diogo0602x <diogoteixeira0602@gmail.com>
2021-06-21 08:48:37 -0300

    Aula 6 - Atividade 10 terminada

--- conta.py
+++ conta.py
@@ -1,51 +1,52 @@
-class  Conta:
+class Conta:
 
-  def __init__(self, numero, titular, saldo, limite):
-    print("Contruindo objeto ... {}".format(self))
-    self.__numero = numero
-    self.__titular = titular
-    self.__saldo = saldo
-    self.__limite = limite
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
-  def extrato(self):
-    print("Saldo de {} de titular {}".format(self.__saldo, self.__titular))
-  
-  def deposita(self, valor):
-    self.__saldo += valor
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
-  def pode_sacar(self):
-    pass
+    def deposita(self, valor):
+        self.__saldo += valor
 
-  def saca(self, valor):
-    if(valor <= (self.__saldo + self.__limite)):
-      self.__saldo -= valor
-    else:
-      print("O valor {} passou o limite".format(valor))
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
 
-  def transfere(self, valor, origem, destino):
-    self.saca(valor)
-    destino.deposita[valor]
+    def saca(self, valor):
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
 
-  @property
-  def saldo(self):
-    return self.__saldo
-  
-  @property
-  def titular(self):
-    return self.__titular
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
 
-  @property
-  def limite(self):
-    return self.__limite
+    @property
+    def saldo(self):
+        return self.__saldo
 
-  @limite.setter
-  def limite(self, limite):
-    self.__limite = limite
+    @property
+    def get_titular(self):
+        return self.__titular
 
-  @staticmethod
-  def codigo_banco():
-    return "001"
+    @property
+    def limite(self):
+        return self.__limite
 
-  @staticmethod
-  def codigos_bancos():
-    return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
--- teste.py
+++ teste.py
@@ -1,12 +1,12 @@
 def cria_conta(numero, titular, saldo, limite):
-   conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
-   return conta
+    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
+    return conta
 
 def deposita(conta, valor):
-  conta["saldo"] += valor 
+    conta["saldo"] += valor
 
 def saca(conta, valor):
-  conta["saldo"] -= valor
+    conta["saldo"] -= valor
 
 def extrato(conta):
-  print("Saldo é ".format(conta["saldo"]))
+    print("Saldo {}".format(conta["saldo"]))
